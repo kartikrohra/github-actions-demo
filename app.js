fc88147d03209c39a001c5aa39e899f1b7a0a294
kartikrohra <kartikrohra2006@gmail.com>
2026-08-17 11:12:36 +0530

    Add temperature conversion functions in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+console.log("25°C =", celsiusToFahrenheit(25), "°F");
+console.log("77°F =", fahrenheitToCelsius(77), "°C");
+
+module.exports = { celsiusToFahrenheit, fahrenheitToCelsius };
